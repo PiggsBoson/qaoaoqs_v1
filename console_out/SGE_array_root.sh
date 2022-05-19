075d83a6f29c04277a20e2918f5d9b96ed66d6f8
Zhibo Yang <zyangaj@gmail.com>
2022-05-18 17:30:46 -0700

    update

--- console_out/SGE_array_root.sh
+++ console_out/SGE_array_root.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -t 1-3
+#$ -t 1,5,6,50
 #$ -cwd             # Current working directory
 #$ -N QAOA_array      # Name of submitted job
 #$ -S /bin/bash     # Shell for execution
@@ -8,10 +8,9 @@
 
 #$ -j y             # Combine output and error streams into single file
 
-# usage example: qsub -t 1-3 array_job_bla.sh
 #Doing everything in the root without involving scratch
 # Redirect all output to log file
-exp_name=PG_rn_Heis_e3_LM_n4eqp60T${SGE_TASK_ID}
+exp_name=Had_PGszXmon_LM_n2eqp20T${SGE_TASK_ID}
 exec > ${exp_name}_${JOB_ID}.log 2>&1
 
 hostname
@@ -19,12 +18,10 @@ date
 echo $SGE_TASK_ID
 echo $JOB_ID
 
-script_path=$HOME/codes/qaoaoqs_v1/qaoaoqs/train.py
-results_path=$HOME/QAOA_results/PG_new
-task_dir=${results_path}/exp/${exp_name}
+script_path=$HOME/qaoaoqs_v1/qaoaoqs/train.py
+results_path=$HOME/QAOA_results/tests/
 
-conda run -n qrl python $script_path --exp_name $exp_name --path $results_path --p 60 --num_iters 2000 -lr 1e-2 --testcase cs_au --env_dim 4 --lr_decay -b 2048 -e 3 --au_uni ST --cs_coup eq --distribution logit-normal --protocol_renormal True --impl quspin --T_tot $SLURM_ARRAY_TASK_ID --scale 1.0
-
+conda run -n qaoa python $script_path --exp_name $exp_name --path $results_path --p 20 --num_iters 2000 -lr 1e-2 --testcase XmonTLS --env_dim 2 --lr_decay -b 2048 -e 5 --au_uni Had --cs_coup uneq --distribution logit-normal --protocol_renormal True --impl quspin --T_tot $SLURM_ARRAY_TASK_ID --scale 1.0
 # Create file to show that job is completed
 touch a_COMPLETED_ti${SGE_TASK_ID}.ji$JOB_ID.log
 

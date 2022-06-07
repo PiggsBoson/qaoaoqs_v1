74c49ca12c72618130eb44f829690ced6a26001d
Zhibo Yang <zhibo_w_yang@ln001.brc>
2022-06-07 01:26:57 -0700

    bug fix

--- console_out/savio_array.sh
+++ console_out/savio_array.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=QAOA_array
-#SBATCH --array=40
+#SBATCH --array=1,2,3,4,5,6,10,20,30,40,50,60
 #
 #SBATCH --account=fc_vaet
 #
@@ -14,7 +14,7 @@
 #
 #SBATCH --time=72:00:00
 
-exp_name=Had_PGszXmon_LM_n2eqp20T
+exp_name=Kochpp_Had_n0T
 #SBATCH --output=${exp_name}%a_%A.log
 
 hostname
@@ -22,9 +22,14 @@ date
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
 script_path=/global/home/users/zhibo_w_yang/codes/qaoaoqs_v1/run/train.py
-results_path=/global/scratch/users/zhibo_w_yang/PGQAOA_results/
+results_path=/global/scratch/users/zhibo_w_yang/PGQAOA_results/Koch_paper
 
+source ~/.bashrc
+module purge #Kill all active modules
 conda init bash
+conda deactivate
+conda deactivate
 conda activate qrl
 
-python $script_path --exp_name $exp_name${SLURM_ARRAY_TASK_ID} --path $results_path --p 20 --num_iters 2000 -lr 1e-2 --testcase XmonTLS --env_dim 2 --lr_decay -b 2048 -e 5 --au_uni Had --cs_coup uneq --distribution logit-normal --protocol_renormal True --impl quspin --T_tot $SLURM_ARRAY_TASK_ID --scale 1.0
+python $script_path --exp_name $exp_name${SLURM_ARRAY_TASK_ID} --path $results_path --p 20 --num_iters 2000 -lr 1e-2 --testcase Koch_paper_1qb_noLind --env_dim 0 --lr_decay -b 2048 -e 5 --au_uni Had --cs_coup eq --distribution logit-normal --protocol_renormal True --impl quspin --T_tot $SLURM_ARRAY_TASK_ID --scale 1.0
+
